13b15906ae02d7cde059b0cc6f0f40726a932cc9
Jhoana Aucancela <j.aucancela6413@gmail.com>
2021-02-02 21:38:11 +0000

    Busqueda_Lineal

--- main.cpp
+++ main.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-#define tam = 50
+#define tam 50
 
+void ingresarElementosArreglo(int arr[], int t);
+int busqueda(int arr[], int t, int dato);
+void imprimirElementosArreglo(int arr[], int t);
 
 
 int main() {
-  int arreglo[]={6,2,7,1,9,10,3,4,5,8};
-  int n, aux=0;
   ofstream archivo;
   archivo.open("demo.txt", ios::app);
 
-  for(int j=0;j<10;j++){
-    archivo<<"["<<arreglo[j]<<"]";
-  }
-  archivo<<endl;
-  cout<<"Ingrese el numero a buscar [1-10]: ";
-  cin>>n;
-  archivo<<"El numero a buscar: "<<n<<endl;
-  for(int i = 0; i<10;i++){
-    if(arreglo[i]==n){
-      cout<<"Numero encontrado en la posicion "<<i+1<<endl;
-      aux =1;
-      archivo<<"Posicion: "<<i<<endl<<endl;
+  int arreglo[tam];
+  int t,dato;
+  cout<<"Ingrese el tamaño del arreglo: ";
+  cin>>t;
+  ingresarElementosArreglo(arreglo,t);
+  cout<<"Ingrese el numero que desea buscar: ";
+  cin>>dato;
+  archivo<<"El numero a buscar: "<<dato<<endl;
+  if(busqueda(arreglo, t, dato)==-1){
+      cout<<"Elemento no encontrado";
+      archivo<<"Elemento no encontrado\n";
     }
-  }
-  if(aux==0){
-    cout<<"Numero no encontrado";
-    archivo<<"Numero no encontrado";
-  }
+  else {cout<<"Elemento encontrado en la posicion: "<<busqueda(arreglo, t, dato)<<endl;
+  archivo<<"Elemento encontrado en la posicion: "<<busqueda(arreglo, t, dato)<<endl<<endl;}
+  imprimirElementosArreglo(arreglo, t);
 
 archivo.close();
+}
 
+
+
+void ingresarElementosArreglo(int arr[], int t){
+  ofstream archivo;
+  archivo.open("demo.txt", ios::app);
+  for(int i = 0; i<t;i++){
+    cout<<"Ingrese el elemento "<<i<<": ";
+    cin>>arr[i];
+    archivo<<"["<<arr[i]<<"]";
+  }
+  archivo<<endl;
+  archivo.close();
+}
+
+int busqueda(int arr[], int t, int dato){
+  int aux =  -1;
+  for(int i = 0; i<t;i++){
+    if(arr[i]==dato){
+      aux = i;
+    }
+  }
+  return aux;
+}
+
+void imprimirElementosArreglo(int arr[], int t){
+   for(int i = 0; i<t;i++){
+    cout<<"["<<arr[i]<<"] ";   
+  }
 }
